6b7a320cb19a54ca85f6a68d830d8dedf10ef79d
Eugene <k2j3.statix@gmail.com>
2018-09-07 16:20:24 +0300

    Fix

--- ListOK/main.js
+++ ListOK/main.js
@@ -1,15 +1,12 @@
 var count = 0;
 function insertIt(item){
-    
-        document.getElementById("maininput").value = "";
-        var numLi = document.createElement('li');
-        var newLi = document.createElement('li');
-        newLi.innerHTML = item;
-        count+=1;
-        numLi.innerHTML = count;
+  document.getElementById("maininput").value = "";
+  var numLi = document.createElement('li');
+  var newLi = document.createElement('li');
+  newLi.innerHTML = item;
+  count++;
+  numLi.innerHTML = count;
 
-        lest.appendChild(newLi);
-        lestnum.appendChild(numLi);
-    }
-
-    
+  lest.appendChild(newLi);
+  lestnum.appendChild(numLi);
+}
